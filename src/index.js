a4a266a95a2bddd1d7b851e6b06ec94085fd5e9f
kkaliuzhny <119534444+kkaliuzhny@users.noreply.github.com>
2023-02-12 20:32:13 +0300

    feat: final made brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, ''); //возвращает в формате (){}[]
+  let mystack = [];
+  for (let i of str) 
+  {
+    let k = brackets.indexOf(i);
+    if (k % 2 === 0) //проверка открывающая скобка или нет
+    {
+      if (i === brackets[k + 1] && mystack[mystack.length - 1] === k) { mystack.pop(); } 
+      else { mystack.push(k) }
+    } 
+    else if (mystack.pop() !== k-1)
+     { return false; }   
+  }
+  return mystack.length === 0;
 }
